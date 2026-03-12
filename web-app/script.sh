ba852d1988bd341d6627bd37d37080e2c52d4028
Ale Paredes <1709578+ale7714@users.noreply.github.com>
2026-03-11 21:50:16 -0400

    Replace web-app script.sh with Go binary entrypoint (#47)

--- web-app/script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Placeholder entrypoint required by the Viam module registry.
-# This is a static web app served via the applications framework.
-exec echo "This module is a static web application."
